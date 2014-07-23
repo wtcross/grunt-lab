7958308575446abed79df3af1f2f5086fc836874
Tyler Cross <tcross@bandwidth.com>
2014-07-23 12:27:50 -0400

    Testing

--- test/lab.js
+++ test/lab.js
@@ -5,9 +5,9 @@ var runTask = require("grunt-run-task");
 var sinon = require("sinon");
 var path = require("path");
 var grunt = require("grunt");
-// var q = require("q");
 
 var describe = Lab.experiment;
+var before = Lab.before;
 var beforeEach = Lab.beforeEach;
 var afterEach = Lab.afterEach;
 var it = Lab.test;
@@ -20,7 +20,6 @@ describe("grunt-lab plugin", function () {
 
 	beforeEach(function (done) {
 		spawnStub = sinon.stub(grunt.util, "spawn").callsArg(1);
-
 		done();
 	});
 
@@ -30,12 +29,21 @@ describe("grunt-lab plugin", function () {
 	});
 
 	describe("properties", function () {
-		var task = runTask.task("lab");
+		var task;
+
+		before(function (done) {
+			task = runTask.task("lab");
+			done();
+		});
 
 		beforeEach(function (done) {
 			task.run(function () {
 				done();
 			});
+		});
+
+		afterEach(function (done) {
+			task.clean(done);
 		});
 
 		it("is not a multitask", function (done) {
@@ -50,11 +58,20 @@ describe("grunt-lab plugin", function () {
 	});
 
 	describe("lab cli execution", function () {
-		describe("with default options", function () {
-			var task = runTask.task("lab");
+		describe("with no configuration", function () {
+			var task;
+
+			before(function (done) {
+				task = runTask.task("lab");
+				done();
+			});
 
 			beforeEach(function (done) {
 				task.run(done);
+			});
+
+			afterEach(function (done) {
+				task.clean(done);
 			});
 
 			it("executes the correct command", function (done) {
@@ -63,6 +80,44 @@ describe("grunt-lab plugin", function () {
 				expect(spawnStub.firstCall.args[0]).to.deep.equal({
 					cmd  : path.join(__dirname, "../node_modules", ".bin", "lab"),
 					args : [ "test/lab.js" ],
+					opts : { stdio : "inherit" }
+				});
+
+				done();
+			});
+		});
+
+		describe("with configuration", function () {
+			var task;
+
+			before(function (done) {
+				task = runTask.task("lab", {
+					coverage    : true,
+					color       : true,
+					parallel    : true,
+					reporter    : "console",
+					minCoverage : 100
+				});
+				done();
+			});
+
+			beforeEach(function (done) {
+				task.run(done);
+			});
+
+			afterEach(function (done) {
+				task.clean(done);
+			});
+
+			it("executes the correct command", function (done) {
+				expect(spawnStub.calledOnce).to.equal(true);
+
+				expect(spawnStub.firstCall.args[0]).to.deep.equal({
+					cmd  : path.join(__dirname, "../node_modules", ".bin", "lab"),
+					args : [
+						"-c", "-C", "-p", "-r",
+						"console", "-t", 100, "test/lab.js"
+					],
 					opts : { stdio : "inherit" }
 				});
 
